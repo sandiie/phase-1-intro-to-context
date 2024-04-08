896a501306274c7bd5d47b0749ccf39d75a2d7e8
+ <+>
2024-04-09 01:40:48 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,92 @@
 // Your code here
+index.js
+function createEmployeeRecord(ary){
+    let record
+    return record = { 
+        firstName: ary[0],
+        familyName: ary[1],
+        title: ary[2],
+        payPerHour: ary[3],
+        timeInEvents: [], 
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(arys){
+    return arys.map(createEmployeeRecord)
+}
+
+function createDSObj(getType, dateStamp) {
+    return {type: getType, date: dateStamp.slice(0,10), hour: parseInt(dateStamp.slice(-4))}
+}
+
+function createTimeInEvent(obj, dateStamp){
+    obj.timeInEvents.push(createDSObj("TimeIn", dateStamp))
+    return obj
+}
+
+
+function createTimeOutEvent(obj, dateStamp){
+    obj.timeOutEvents.push(createDSObj("TimeOut", dateStamp))
+    return obj
+}
+
+function hoursWorkedOnDate(obj, dateYMD){
+    let timeIn = obj.timeInEvents.find((e) => e.date === dateYMD).hour
+    let  timeOut = obj.timeOutEvents.find((e) => e.date === dateYMD).hour
+    return (timeOut - timeIn)/100
+}
+
+function wagesEarnedOnDate(obj, dateYMD){
+
+    const wage = obj.payPerHour
+    const hoursWorked = hoursWorkedOnDate(obj, dateYMD)
+    return wage * hoursWorked
+}
+
+function allWagesFor(obj){
+    const allWages = obj.timeInEvents.map((day) => {return wagesEarnedOnDate(obj, day.date)})
+    return allWages.reduce((acc, cv) => acc + cv)
+}
+
+function calculatePayroll(records){
+    const allPay = (records.map((empl) => {return allWagesFor(empl)}))
+    return allPay.reduce((acc, cv) => acc + cv)
+}
+
+function findEmployeeByFirstName(srcArray, first_Name){
+    return srcArray.find((record) => record.firstName === first_Name)
+}
+
+
+
+
+        let rRecord = createEmployeeRecord(["Rafiki", "", "Aide", 10])
+        let sRecord = createEmployeeRecord(["Simba", "", "King", 100])
+
+        let  sTimeData = [
+          ["2019-01-01 0900", "2019-01-01 1300"], 
+          ["2019-01-02 1000", "2019-01-02 1300"]  
+        ]
+
+        let rTimeData = [
+          ["2019-01-11 0900", "2019-01-11 1300"], 
+          ["2019-01-12 1000", "2019-01-12 1300"]  
+        ]
+
+        sTimeData.forEach(function (d) {
+          const [dIn, dOut] = d
+          sRecord = createTimeInEvent(sRecord, dIn)
+          sRecord = createTimeOutEvent(sRecord, dOut)
+        })
+
+        rTimeData.forEach(function (d, i) {
+          const [dIn, dOut] = d
+          rRecord = createTimeInEvent(rRecord, dIn)
+          rRecord = createTimeOutEvent(rRecord, dOut)
+        })
+
+        const employees = [sRecord, rRecord]
+
+        calculatePayroll(employees)
+Footer
